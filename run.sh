9492210b96d9087033f869c6577f444016c631ce
vetleras <vetleras@gmail.com>
2022-10-06 15:16:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # use with "sudo bash run.sh <name of the file with no extension>"
 
-if sudo javac -classpath .:classes:/opt/pi4j/lib/'*' $1.java; then
+if sudo javac -classpath .:classes:/opt/pi4j/lib/'*' -cp *.jar $1.java; then
     echo "Java program '$1' successfully compiled"
-    sudo java -classpath .:classes:/opt/pi4j/lib/'*' $1
+    sudo java -classpath .:classes:/opt/pi4j/lib/'*' -cp *.jar $1
 else
     echo "Java program '$1' compilation failed"
 fi
